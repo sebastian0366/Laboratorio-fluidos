4e07b241824d7f6341fa35eebc2579936512f845
sebastian0366 <77077239+sebastian0366@users.noreply.github.com>
2026-09-25 15:47:57 -0500

    app.js

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ let hVal, bVal, thetaVal, wVal, gammaVal;
 let frOut, cpOut, tOut;
 
 // Constantes Físicas
-const L = 2.5;       
+const L = 3.5;       
 
 function setup() {
     let canvasContainer = select('#canvas-container');
